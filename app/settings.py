2a14229feec213bad3973729438f0781dde00b35
SergDeLaVega <sergkolot84@gmail.com>
2024-06-07 12:36:00 +0300

    создана система регистрации и авторизации

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,6 +47,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -142,6 +145,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/users/login/  '
 
 INTERNAL_IPS = [
     # ...
